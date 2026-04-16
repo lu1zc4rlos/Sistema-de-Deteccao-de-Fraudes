6f8fa80b8c436800418c3069fc98b355eeac417e
lu1zc4rlos <jujuni1000.carlos@gmail.com>
2026-04-16 18:55:20 -0300

    chore: initial project setup

---
+++ fraud-detection-api/src/main/java/com/luiz/frauddetection/FraudDetectionApiApplication.java
@@ -0,0 +1,13 @@
+package com.luiz.frauddetection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudDetectionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudDetectionApiApplication.class, args);
+	}
+
+}
---
+++ fraud-detection-api/src/test/java/com/luiz/frauddetection/FraudDetectionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luiz.frauddetection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudDetectionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
