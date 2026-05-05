9f7e4cb837c54d467c90dc5ed0b25ae26bd4614f
lu1zc4rlos <jujuni1000.carlos@gmail.com>
2026-05-05 18:38:09 -0300

    feat: implementar autenticação JWT e fluxo de segurança do usuário
    - Implementação da interface UserDetails na entidade User
    - Criação da classe UserDetailsServiceImpl
    - Criação dos métodos save e findByEmail no UserRepository
    - Criação do JwtService com geração e validação de tokens

--- fraud-detection-api/src/main/java/com/luiz/frauddetection/repository/UserRepository.java
+++ fraud-detection-api/src/main/java/com/luiz/frauddetection/repository/UserRepository.java
@@ -4,6 +4,10 @@ import com.luiz.frauddetection.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    <S extends User> S save(S entity);
+    Optional<User> findByEmail(String email);
 }
---
+++ fraud-detection-api/src/main/java/com/luiz/frauddetection/service/JwtService.java
@@ -0,0 +1,77 @@
+package com.luiz.frauddetection.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtService {
+
+    @Value("${api.security.token.secret}")
+    private String secretKey;
+
+    @Value("${api.security.token.expiration}")
+    private Long jwtExpiration;
+
+    @Value("${api.security.token.issuer}")
+    private String issuer;
+
+    public String generateToken(UserDetails userDetails) {
+        return Jwts.builder()
+                .setIssuer(issuer)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    public String extractUsername(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    protected boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
